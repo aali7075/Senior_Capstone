4fc3d316fa750a67b752bca218b44785bdcff401
nelsasser <elsanic96@gmail.com>
2021-10-14 12:29:59 -0600

    added y axis function and multiple coils

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Units : A * H / (m * cm), therefore need to multiply by 100x.
@@ -30,16 +31,20 @@ c2 = lambda x, y, z, a1, b1, z0: a1 - x
 c3 = lambda x, y, z, a1, b1, z0: -a1 + x
 c4 = lambda x, y, z, a1, b1, z0: -a1 - x
 
+
 def _eval_r(x, y, z, a1, b1, z0):
     return r1(x, y, z, a1, b1, z0), r2(x, y, z, a1, b1, z0), r3(x, y, z, a1, b1, z0), r4(x, y, z, a1, b1, z0)
+
 
 def _eval_d(x, y, z, a1, b1, z0):
     return d1(x, y, z, a1, b1, z0), d2(x, y, z, a1, b1, z0), d3(x, y, z, a1, b1, z0), d4(x, y, z, a1, b1, z0)
 
+
 def _eval_c(x, y, z, a1, b1, z0):
     return c1(x, y, z, a1, b1, z0), c2(x, y, z, a1, b1, z0), c3(x, y, z, a1, b1, z0), c4(x, y, z, a1, b1, z0)
 
-def Bz1(x, y, z, a1, b1, z0, N):
+
+def Bz(x, y, z, a1, b1, z0, N):
     """
     Magnetic flux density in Z direction at point (x, y, z)
 
@@ -49,31 +54,25 @@ def Bz1(x, y, z, a1, b1, z0, N):
     :param a1: (half of) coil side length A
     :param b1: (half of) coil side length B
     :param z0: Distance of coil from origin
-    :param N: ???
+    :param N: Current (Milliamps?)
 
     :return: Magnetic flux density
     """
-    c = 100.0 * 1e6 * MU_0 * N / (4 * np.pi)
+    _k = 100.0 * 1e6 * MU_0 * N / (4 * np.pi)
 
     # compute each sub-function once and store result
-    _r1, _r2, _r3, _r4 = _eval_r(x, y, z, a1, b1, z0)
-    _d1, _d2, _d3, _d4 = _eval_d(x, y, z, a1, b1, z0)
-    _c1, _c2, _c3, _c4 = _eval_c(x, y, z, a1, b1, z0)
+    _r = _eval_r(x, y, z, a1, b1, z0)
+    _d = _eval_d(x, y, z, a1, b1, z0)
+    _c = _eval_c(x, y, z, a1, b1, z0)
 
     # return constant * inner sum of various function combinations
     # tried to keep things clean, or at least cleaner than the original mathematica code
-    return c * np.sum([
-        -1.0 *  _d1 / (_r1 * (_r1 + _c1)),
-        -1.0 *  _c1 / (_r1 * (_r1 + _d1)),
-                _d2 / (_r2 * (_r2 - _c2)),
-        -1.0 *  _c2 / (_r2 * (_r2 + _d2)),
-        -1.0 *  _d3 / (_r3 * (_r3 + _c3)),
-        -1.0 *  _c3 / (_r3 * (_r3 + _d3)),
-                _d4 / (_r4 * (_r4 - _c4)),
-        -1.0 *  _c4 / (_r4 * (_r4 + _c4))
-    ], axis=0)
 
-def Bx1(x, y, z, a1, b1, z0, N):
+    return _k * np.sum([((-1**(i+1))*_d[i] / (_r[i] * (_r[i] + (-1**i)*_c[i]))) -
+                        (_c[i] / (_r[i] * (_r[i] - _d[i]))) for i in range(4)], axis=0)
+
+
+def Bx(x, y, z, a1, b1, z0, N):
     """
     Magnetic flux density in X direction at point (x, y, z)
 
@@ -83,32 +82,84 @@ def Bx1(x, y, z, a1, b1, z0, N):
     :param a1: (half of) coil side length A
     :param b1: (half of) coil side length B
     :param z0: Distance of coil from origin
-    :param N: ???
+    :param N: Current (Milliamps?)
 
     :return: Magnetic flux density
     """
 
-    c = 100.0 * MU_0 * N / (4 * np.pi)
-
+    _k = 100.0 * MU_0 * N / (4 * np.pi)
     _z = z - z0
-    _r1, _r2, _r3, _r4 = _eval_r(x, y, z, a1, b1, z0)
-    _d1, _d2, _d3, _d4 = _eval_d(x, y, z, a1, b1, z0)
+    _r = _eval_r(x, y, z, a1, b1, z0)
+    _d = _eval_d(x, y, z, a1, b1, z0)
 
-    return c * np.sum([
-                _z / (_r1 * (_r1 + _d1)),
-        -1.0 *  _z / (_r2 * (_r2 + _d2)),
-                _z / (_r3 * (_r3 + _d3)),
-        -1.0 *  _z / (_r4 * (_r4 + _d4))
-    ], axis=0)
+    return _k * np.sum([(-1**i) * _z / (_r[i] * (_r[i] + _d[i])) for i in range(4)], axis=0)
+
+def By(x, y, z, a1, b1, z0, N):
+    """
+    Magnetic flux density in X direction at point (x, y, z)
+
+    :param x: x location
+    :param y: y location
+    :param z: z location
+    :param a1: (half of) coil side length A
+    :param b1: (half of) coil side length B
+    :param z0: Distance of coil from origin
+    :param N: Current (Milliamps?)
+
+    :return: Magnetic flux density
+    """
+
+    _k = 100.0 * MU_0 * N / (4 * np.pi)
+    _z = z - z0
+
+    _r = _eval_r(x, y, z, a1, b1, z0)
+    _c = _eval_c(x, y, z, a1, b1, z0)
+
+    return _k * np.sum([(-1**i) * _z / (_r[i] * (_r[i] + (-1**i) * _c[i])) for i in range(4)], axis=0)
+
+def get_coordinates(a1, b1, s, N, x, y):
+    # get (x, y) coordinates given spacing between coils, coil sizes,
+    # and number of coils s.t. the center of the coils is the origin
+
+    f = lambda x: (s * (N-1) + 2 * x * (N-1)) / 2.0
+
+    f_a1, f_b1 = f(a1), f(b1)
+    xx = np.linspace(-1.0 * f_a1 + x, f_a1 + x, num=N)
+    yy = np.linspace(-1.0 * f_b1 + y, f_b1 + y, num=N)
+
+    return np.meshgrid(xx, yy)
+
+def get_gradient(x, y, z, a1, b1, z0, current, num_coils, coil_spacing):
+    xx, yy = get_coordinates(a1, b1, coil_spacing, num_coils, x, y)
+
+    bx = Bx(x, y, z, a1, b1, z0, current)
+    by = By(x, y, z, a1, b1, z0, current)
+    bz = Bz(x, y, z, a1, b1, z0, current)
+
+    return np.array([bx, by, bz])
 
 if __name__ == '__main__':
     # define some random constants to plot
-    Z = np.linspace(-5, 5, num=100)
-    X, Y, A1, B1, Z0, N = 0, 0, 3.5, 3.5, 1.75, 12
-    B = Bz1(X, Y, Z, A1, B1, Z0, N)
+    x_axis = np.linspace(-10, 10, num=100)
+    y_axis = np.linspace(-10, 10, num=100)
+    Z, A1, B1, Z0, I = 1.0, 2, 2, 0.0, 12
 
-    plt.plot(Z, B)
-    plt.title('Single coil')
-    plt.xlabel('Z (cm)')
-    plt.ylabel('Bz field strength (nT/mA)')
+    grid = np.zeros((len(x_axis), len(y_axis)))
+    for i, X in enumerate(x_axis):
+        B = get_gradient(X, y_axis, Z, A1, B1, Z0, I, num_coils=1, coil_spacing=1.0)
+        grid[i] = np.linalg.norm(B, axis=0)
+
+    ax = sns.heatmap(grid)
     plt.show()
+
+    # density = np.zeros(len(Z))
+    # spacing = 1.0
+    # GRID_SIZE = 3
+    #
+    # gx, gy = get_coordinates(A1, B1, spacing, GRID_SIZE)
+    # for i in range(GRID_SIZE):
+    #     for j in range(GRID_SIZE):
+    #         density += Bz1(gx[i, j], gy[i, j], Z, A1, B1, Z0, N)
+    #
+    # plt.plot(Z, density, label=f'multi-coils ({GRID_SIZE}x{GRID_SIZE})')
+    # plt.show()
