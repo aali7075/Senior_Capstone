a7d0db5b7db0fdace480f751b57712fef47287cd
nelsasser <elsanic96@gmail.com>
2021-10-15 10:10:40 -0600

    added coil overlays in x,y plane view

--- src/main.py
+++ src/main.py
@@ -58,6 +58,27 @@ def Bz(x, y, z, a1, b1, z0, N):
 
     :return: Magnetic flux density
     """
+
+    # c = 100.0 * 1e6 * MU_0 * N / (4 * np.pi)
+    #
+    # # compute each sub-function once and store result
+    # _r1, _r2, _r3, _r4 = _eval_r(x, y, z, a1, b1, z0)
+    # _d1, _d2, _d3, _d4 = _eval_d(x, y, z, a1, b1, z0)
+    # _c1, _c2, _c3, _c4 = _eval_c(x, y, z, a1, b1, z0)
+    #
+    # # return constant * inner sum of various function combinations
+    # # tried to keep things clean, or at least cleaner than the original mathematica code
+    # return c * np.sum([
+    #     -1.0 *  _d1 / (_r1 * (_r1 + _c1)),
+    #     -1.0 *  _c1 / (_r1 * (_r1 + _d1)),
+    #             _d2 / (_r2 * (_r2 - _c2)),
+    #     -1.0 *  _c2 / (_r2 * (_r2 + _d2)),
+    #     -1.0 *  _d3 / (_r3 * (_r3 + _c3)),
+    #     -1.0 *  _c3 / (_r3 * (_r3 + _d3)),
+    #             _d4 / (_r4 * (_r4 - _c4)),
+    #     -1.0 *  _c4 / (_r4 * (_r4 + _c4))
+    # ], axis=0)
+
     _k = 100.0 * 1e6 * MU_0 * N / (4 * np.pi)
 
     # compute each sub-function once and store result
@@ -132,34 +153,84 @@ def get_coordinates(a1, b1, s, N, x, y):
 def get_gradient(x, y, z, a1, b1, z0, current, num_coils, coil_spacing):
     xx, yy = get_coordinates(a1, b1, coil_spacing, num_coils, x, y)
 
-    bx = Bx(x, y, z, a1, b1, z0, current)
-    by = By(x, y, z, a1, b1, z0, current)
-    bz = Bz(x, y, z, a1, b1, z0, current)
+    bx, by, bz = None, None, None
+    for i in range(num_coils):
+        for j in range(num_coils):
+            if bx is None:
+                bx = Bx(xx[i, j], yy[i, j], z, a1, b1, z0, current)
+            else:
+                bx += Bx(xx[i, j], yy[i, j], z, a1, b1, z0, current)
+
+            if by is None:
+                by = By(xx[i, j], yy[i, j], z, a1, b1, z0, current)
+            else:
+                by += By(xx[i, j], yy[i, j], z, a1, b1, z0, current)
+
+            if bz is None:
+                bz = Bz(xx[i, j], yy[i, j], z, a1, b1, z0, current)
+            else:
+                bz += Bz(xx[i, j], yy[i, j], z, a1, b1, z0, current)
 
     return np.array([bx, by, bz])
+
+def overlay_coils(a1, b1, num_coils, coil_spacing, unit, ax):
+    # convert coil sizes to matplotlib units
+    a1 *= unit
+    b1 *= unit
+    coil_spacing *= unit
+
+    f = lambda x: (coil_spacing * (num_coils - 1) + 2 * x * (num_coils - 1)) / 2.0
+
+    f_a1, f_b1 = f(a1), f(b1)
+
+    # shift up and over s.t. we have the top left corner of each coil
+    xp = np.linspace(-1.0 * f_a1 + 50, f_a1 + 50, num=num_coils) - a1
+    yp = np.linspace(-1.0 * f_b1 + 50, f_b1 + 50, num=num_coils) - b1
+    xx, yy = np.meshgrid(xp, yp)
+    for i in range(num_coils):
+        for j in range(num_coils):
+            px, py = xx[i, j], yy[i, j]
+            print(px, py, 2*a1, 2*b1)
+            rect = plt.Rectangle((px, py), 2*a1, 2*b1, fc='none', ec='green', lw=2.0, alpha=0.75)
+            ax.add_patch(rect)
 
 if __name__ == '__main__':
     # define some random constants to plot
     x_axis = np.linspace(-10, 10, num=100)
     y_axis = np.linspace(-10, 10, num=100)
-    Z, A1, B1, Z0, I = 1.0, 2, 2, 0.0, 12
+    A1, B1, Z0, I, spacing = 2, 2, 0.0, 1, 1.0
+    Z = 1.0
 
-    grid = np.zeros((len(x_axis), len(y_axis)))
-    for i, X in enumerate(x_axis):
-        B = get_gradient(X, y_axis, Z, A1, B1, Z0, I, num_coils=1, coil_spacing=1.0)
-        grid[i] = np.linalg.norm(B, axis=0)
+    GRID = 3
+    xx, yy = get_coordinates(A1, B1, spacing, GRID, 0, 0)
+    size = max((GRID-1), 1) * A1 * spacing * 2
 
-    ax = sns.heatmap(grid)
+    grid = np.zeros((100, 100))
+    x_axis = np.linspace(-size, size, 100)
+    y_axis = np.linspace(-size, size, 100)
+    unit = 100 / (2*size) # convert cm grid (2*size x 2*size) to matplotlib unit grid (100x100 plot)
+    for i, x in enumerate(x_axis):
+        for j, y in enumerate(y_axis):
+            # visualize coil placement by plotting distance to closest coil for each (x,y)
+
+            # v = [np.sqrt((x - xx[ii, jj])**2 + (y - yy[ii, jj])**2)
+            #     for ii in range(GRID) for jj in range(GRID)]
+            # grid[i, j] = min(v)
+
+            # get the magnitude of the field at each x, y
+
+            B = get_gradient(x, y, Z, A1, B1, Z0, I, GRID, spacing)
+            grid[i, j] = np.linalg.norm(B, axis=0)
+
+    # plot the grid
+    xticks = ['' if i % 10 else f'{x_axis[i]:.2f}' for i in range(len(x_axis))][:-1] + [x_axis[-1]]
+    yticks = ['' if i % 10 else f'{y_axis[i]:.2f}' for i in range(len(y_axis))][:-1] + [y_axis[-1]]
+    ax = sns.heatmap(grid, xticklabels=xticks, yticklabels=yticks)
+
+    overlay_coils(A1, B1, GRID, spacing, unit, ax)
+
+    plt.title(f'Magnetic Field Magnitude (nT) of a {GRID}x{GRID} of Coils at Z={Z}cm.')
+    plt.xlabel('X position (cm)')
+    plt.ylabel('Y position (cm)')
+
     plt.show()
-
-    # density = np.zeros(len(Z))
-    # spacing = 1.0
-    # GRID_SIZE = 3
-    #
-    # gx, gy = get_coordinates(A1, B1, spacing, GRID_SIZE)
-    # for i in range(GRID_SIZE):
-    #     for j in range(GRID_SIZE):
-    #         density += Bz1(gx[i, j], gy[i, j], Z, A1, B1, Z0, N)
-    #
-    # plt.plot(Z, density, label=f'multi-coils ({GRID_SIZE}x{GRID_SIZE})')
-    # plt.show()
