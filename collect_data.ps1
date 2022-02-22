c3da6602d4d7c63c7465a08416ff58c8b7def131
lab <labtop@cu.com>
2022-02-22 11:20:35 -0700

    fixed path

--- collect_data.ps1
+++ collect_data.ps1
@@ -1,4 +1,4 @@
 $ts = Get-Date -Format "yyyy-mm-ddTHH-mm-ss"
-$fname = "C:\\Users\\Lab\\Documents\\python\\python_" + $ts + ".log"
+$fname = "C:\\Users\\Lab\\Documents\\logs\\python\\python_" + $ts + ".log"
 
 C:\Users\Lab\.virtualenvs\Senior_Capstone-ZalnXfov\Scripts\python.exe C:\Users\Lab\Desktop\Senior_Capstone\labview\niTest.py > $fname
